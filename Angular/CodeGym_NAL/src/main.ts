6df1464f70c70859311d8fcbe2c8aca2ae0ee7c2
KatoIT <nguyenvananymnb@gmail.com>
2021-12-13 15:21:41 +0700

    update

--- Angular/CodeGym_NAL/src/main.ts
+++ Angular/CodeGym_NAL/src/main.ts
@@ -11,9 +11,72 @@ import { environment } from './environments/environment';
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
+/**
+ * [Thực hành] Khai báo và sử dụng biến
+ */
 let width: number;
 let height: number;
 width = 10.5;
 height = 20;
 let area: number = width*height;
-console.log(`Diện tích hình chữ nhật: ${area}`);
+console.log('Diện tích hình chữ nhật: ${area}');
+
+/**
+ * [Thực hành] Câu lệnh điều kiện và vòng lặp
+ */
+let sum: number = 0;
+let count: number = 0;
+for (let i = 2; count < 30; i++) {
+    let isPrime: boolean = true;
+    if (i == 2) {
+        sum += i;
+        count++;
+        continue;
+    }
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if(!isPrime){
+        continue;
+    }
+    sum += i;
+    count++;
+}
+console.log('Sum = ' + sum);
+
+/**
+ * [Thực hành] Kiểm tra số nguyên tố
+ */
+function isPrime(number: number): boolean {
+    let isPrime = true;
+    if (number < 2) {
+        isPrime = false;
+    } else if (number > 2) {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
+}
+let array = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+sum = 0;
+for (let number of array) {
+    if (isPrime(number)) {
+        sum += number;
+    }
+}
+console.log("Tổng các số nguyên tố trong mảng trên là: " + sum);
+/**
+ * 
+ */
+import {Rectangle} from "./rectangle";
+
+const rectangle = new Rectangle(5, 5, 10, 20);
+console.log(rectangle.toString());
+console.log(rectangle.area());
