06548f6519b5ed4c62fb9130d49ac60b62a4401a
KatoIT <nguyenvananymnb@gmail.com>
2021-12-13 15:47:41 +0700

    update

--- Angular/typescript-is-prime/main.ts
+++ Angular/typescript-is-prime/main.ts
@@ -70,3 +70,25 @@ const rectangle = new Rectangle(5, 5, 10, 20);
 console.log(rectangle.toString());
 console.log(rectangle.area());
 
+/**
+ * [Thực hành] Promise
+ */
+let money = 10000;
+const buyACar = (car: any) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("can buy " + car);
+            } else {
+                reject("Do not enough money");
+            }
+        }, 100);
+    }))
+}
+
+money = 1000001;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+})
