4fc9700a3ccb3dae7b5d744b94efa03151566375
KatoIT <nguyenvananymnb@gmail.com>
2021-12-21 13:48:03 +0700

    todo_app

--- Angular/Tutorialv2/src/app/service/todo.service.ts
+++ Angular/Tutorialv2/src/app/service/todo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Todo} from "../todo";
+import {Observable} from "rxjs";
+const API_URL = '$(environment.apiUrl)';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(API_URL + '/todo');
+  }
+  saveTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(API_URL + '/todo', todo);
+  }
+
+  findById(id: number): Observable<Todo> {
+    return this.http.get<Todo>(`${API_URL}/todo/${id}`);
+  }
+
+  updateTodo(id: number, Todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${API_URL}/todo/${id}`, Todo);
+  }
+
+  deleteTodo(id: number): Observable<Todo> {
+    return this.http.delete<Todo>(`${API_URL}/todo/${id}`);
+  }
 }
--- Angular/Tutorialv2/src/app/todo/todo.component.ts
+++ Angular/Tutorialv2/src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {Todo} from "../todo";
+import {TodoService} from "../service/todo.service";
 
 let _id = 1;
 
@@ -13,10 +14,18 @@ export class TodoComponent implements OnInit {
   todos: Todo[] = [];
   content = new FormControl();
 
-  constructor() {
-  }
+  constructor(
+    private todoService: TodoService
+  ) {
 
+  }
+  getAll(){
+    this.todoService.getAll().subscribe(value => {
+      this.todos = value;
+    });
+  }
   ngOnInit() {
+    this.getAll();
   }
 
   toggleTodo(i: number) {
@@ -24,16 +33,24 @@ export class TodoComponent implements OnInit {
   }
 
   change() {
-    const value = this.content.value;
-    if (value) {
-      const todo: Todo = {
-        id: _id++,
-        content: value,
-        complete: false
-      };
-      this.todos.push(todo);
+    // const value = this.content.value;
+    // if (value) {
+    //   const todo: Todo = {
+    //     id: _id++,
+    //     content: value,
+    //     complete: false
+    //   };
+    //   this.todos.push(todo);
+    //   this.content.reset();
+    // }
+    const todo = this.content.value;
+    this.todoService.saveTodo(todo).subscribe(() => {
       this.content.reset();
-    }
+      alert('Tạo thành công');
+    }, e => {
+      console.log(e);
+    });
+    this.content.reset();
   }
 
 }
