fcf9f500374a2060574c64d263cd769b6bcad354
KatoIT <nguyenvananymnb@gmail.com>
2021-12-21 17:02:44 +0700

    init group message

--- Angular/Tutorialv2/src/app/service/group.service.ts
+++ Angular/Tutorialv2/src/app/service/group.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Groups} from "../model/groups";
+import {UserInGroup} from "../model/user-in-group";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
+  groups: Groups[] = [
+    {
+      groupId: 0,
+      groupName: 'Group_01',
+      avatar: ''
+    }, {
+      groupId: 1,
+      groupName: 'Group_02',
+      avatar: ''
+    }, {
+      groupId: 2,
+      groupName: 'Group_03',
+      avatar: ''
+    }
+  ]
+  userInGroup: UserInGroup[] = [
+    {
+      groupId: 0,
+      userId: 0
+    }, {
+      groupId: 0,
+      userId: 1
+    }, {
+      groupId: 1,
+      userId: 2
+    }, {
+      groupId: 1,
+      userId: 1
+    }, {
+      groupId: 1,
+      userId: 0
+    }, {
+      groupId: 0,
+      userId: 4
+    }
+  ]
 
-  constructor() { }
+  constructor() {
+  }
+
+  findAllGroupByUserId(userId: number) {
+    for (let group in this.userInGroup) {
+
+    }
+  }
 }
--- Angular/Tutorialv2/src/app/service/user.service.ts
+++ Angular/Tutorialv2/src/app/service/user.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Users} from "../model/users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  users: Users[] = [
+    {
+      userId: 0,
+      avatar: '',
+      nickName: 'KatoIT',
+      status: true
+    }, {
+      userId: 1,
+      avatar: '',
+      nickName: 'User_01',
+      status: false
+    }, {
+      userId: 2,
+      avatar: '',
+      nickName: 'User_02',
+      status: true
+    }, {
+      userId: 3,
+      avatar: '',
+      nickName: 'User_03',
+      status: false
+    }, {
+      userId: 4,
+      avatar: '',
+      nickName: 'User_04',
+      status: true
+    }
+  ]
 
-  constructor() { }
+  constructor() {
+  }
+
+  findUserById(id: number) {
+    return this.users.find(value => value.userId === id)
+  }
+
 }
