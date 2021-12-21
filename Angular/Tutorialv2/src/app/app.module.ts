875115eb9ea19f60059549b096daeb0ae4de8bea
KatoIT <nguyenvananymnb@gmail.com>
2021-12-21 13:25:57 +0700

    todo_app

--- Angular/Tutorialv2/src/app/app.module.ts
+++ Angular/Tutorialv2/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from './app.component';
+import {TodoComponent} from "./todo/todo.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
