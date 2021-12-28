9f2833c945601acdd30a7e5c4aa842cc152b7cd7
KatoIT <nguyenvananymnb@gmail.com>
2021-12-28 10:27:03 +0700

    Create UI

--- Angular/Tutorialv2/src/app/app.module.ts
+++ Angular/Tutorialv2/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import {UsersModule} from "./users/users.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
   ],
   imports: [
     BrowserModule,
-    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
